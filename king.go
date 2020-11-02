3718d89bb912ad0648d20d39d73dca0dc17f0d50
Rene Zbinden <rene.zbinden@postfinance.ch>
2020-11-02 10:20:38 +0100

    move metrics stuff to metrics.go

--- king.go
+++ king.go
@@ -4,13 +4,11 @@ package king
 
 import (
 	"context"
-	"fmt"
 	"regexp"
 	"sort"
 	"strings"
 
 	"github.com/alecthomas/kong"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 const redactChar = `*`
@@ -145,18 +143,6 @@ func (m Map) List() []interface{} {
 	return l
 }
 
-// Register registers prometheus flag collectors that display configured flags as
-// metrics. The genereated metrics are of the form:
-//
-//     kong_flag{program="progname", name="flagname", value="flagvalue"} 1
-func (m Map) Register(program string, registerer prometheus.Registerer) Map {
-	for _, c := range m.collectors(program) {
-		registerer.MustRegister(c)
-	}
-
-	return m
-}
-
 func (m Map) keys() []string {
 	keys := make([]string, 0, len(m))
 
@@ -167,20 +153,6 @@ func (m Map) keys() []string {
 	sort.Strings(keys)
 
 	return keys
-}
-
-func (m Map) collectors(program string) []prometheus.Collector {
-	collectors := []prometheus.Collector{}
-
-	for name, value := range m {
-		if isRedacted(value) {
-			continue
-		}
-
-		collectors = append(collectors, newFlagCollector(program, name, fmt.Sprintf("%v", value)))
-	}
-
-	return collectors
 }
 
 func bindContext(ctx context.Context) kong.Option {
@@ -232,28 +204,4 @@ func newHelpFormatter(appName string) func(*kong.Value) string {
 			return value.Help + " " + suffix
 		}
 	}
-}
-
-func newFlagCollector(program string, flags ...string) prometheus.Collector {
-	return prometheus.NewGaugeFunc(
-		prometheus.GaugeOpts{
-			Name: "kong_flag",
-			Help: "A metric with a constant '1' value labeled by program, flag name and value",
-			ConstLabels: prometheus.Labels{
-				"program": program,
-				"name":    flags[0],
-				"value":   flags[1],
-			},
-		},
-		func() float64 { return 1 },
-	)
-}
-
-func isRedacted(value interface{}) bool {
-	v, ok := value.(string)
-	if !ok {
-		return false
-	}
-
-	return v == strings.Repeat(string(redactChar), len(v))
 }
